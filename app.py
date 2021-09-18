4049a1c4ce698786b4c8f517bac924992a8c6112
jayita19 <87515288+jayita19@users.noreply.github.com>
2021-09-18 20:29:59 +0530

    update

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello Jayita!  This is Udacity final project Blue Deployment version 1"
+    return "Hello Jayita!  This is Udacity final project Green Deployment version 2.1"
 app.run(host="0.0.0.0", port=8080, debug=True)
